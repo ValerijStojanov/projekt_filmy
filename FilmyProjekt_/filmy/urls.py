8adb90ced295c8a8b7a5489368e4b70b9e2a191b
ValerijStojanov <99343041+ValerijStojanov@users.noreply.github.com>
2023-07-18 17:45:31 +0200

    Delete FilmyProjekt_ directory

--- FilmyProjekt_/filmy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from filmy import views
-
-app_name = 'filmy'
-
-urlpatterns = [
-    path('seznam/', views.seznam, name='seznam'),
-    path('detail/<slug:slug>/', views.detail, name='detail'),
-]
